ca50e059ad3c47ef7344b95a6188d07393930075
Raquel <raquel@conecthus.org.br>
2024-12-10 16:14:31 -0400

    feat: alterando services

--- backend/src/products/products.module.ts
+++ backend/src/products/products.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
+import { DatabaseModule } from '../database/database.module';
+import { productProviders } from './products.providers';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [ProductsController],
-  providers: [ProductsService],
+  providers: [...productProviders, ProductsService],
 })
 export class ProductsModule {}
--- backend/src/products/products.service.ts
+++ backend/src/products/products.service.ts
@@ -1,26 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Repository } from 'typeorm';
+import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  constructor(
+    @Inject('PRODUCT_REPOSITORY')
+    private productRepository: Repository<Product>,
+  ) {}
+
+  async create(createProductDto: CreateProductDto) {
+    console.log('This action adds a new product');
+    const product = this.productRepository.create(createProductDto);
+    return this.productRepository.save(product);
   }
 
-  findAll() {
-    return `This action returns all products`;
+  async findAll() {
+    console.log(`This action returns all products`);
+    return this.productRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: string) {
+    console.log(`This action returns a #${id} product`);
+    return this.productRepository.findOneBy({ id });
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: string, updateProductDto: UpdateProductDto) {
+    console.log(`This action updates a #${id} product`);
+    const product = await this.findOne(id);
+    this.productRepository.merge(product, updateProductDto);
+    return this.productRepository.save(product);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: string) {
+    console.log(`This action removes a #${id} product`);
+    const product = await this.findOne(id);
+    return this.productRepository.remove(product);
   }
 }
