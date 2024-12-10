d4a1fba18c8c05cfb32ead7de216e8cd692a553e
Raquel <raquel@conecthus.org.br>
2024-12-10 16:14:01 -0400

    feat: criando entidade

--- backend/src/products/entities/product.entity.ts
+++ backend/src/products/entities/product.entity.ts
@@ -1,1 +1,31 @@
-export class Product {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ length: 500 })
+  name: string;
+
+  @Column()
+  code: string;
+
+  @Column()
+  creator: string;
+
+  @Column()
+  approver: string;
+
+  @Column('datetime')
+  startDate: string;
+
+  @Column('datetime')
+  endDate: string;
+
+  @Column()
+  productFamily: string;
+
+  @Column()
+  finalGood: string;
+}
