1f908dd8b1b1577b7e285e82e81a17464ebc6b26
Raquel <raquel@conecthus.org.br>
2024-12-11 17:09:25 -0400

    feat: implementacao de geracao de token

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -3,9 +3,19 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { LocalStrategy } from './strategies/local.strategy';
+import { JwtModule } from '@nestjs/jwt';
+
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '30d' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService, LocalStrategy],
 })
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import * as bcrypt from 'bcrypt';
+import { Users } from 'src/users/entities/user.entity';
+import { UserPayload } from './models/UserPayload';
+import { JwtService } from '@nestjs/jwt';
+import { UserToken } from './models/UserToken';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UsersService) {}
+  constructor(
+    private readonly userService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async validateUser(email: string, password: string) {
     const user = await this.userService.findByEmail(email);
@@ -14,5 +21,18 @@ export class AuthService {
       if (isPasswordValid) return { ...user, password: undefined };
     }
     throw new Error('Email address or password provided is incorrect');
+  }
+
+  login(user: Users): UserToken {
+    const payload: UserPayload = {
+      email: user.email,
+      sub: user.id,
+      name: user.name,
+    };
+
+    const jwtToken = this.jwtService.sign(payload);
+    return {
+      access_token: jwtToken,
+    };
   }
 }
