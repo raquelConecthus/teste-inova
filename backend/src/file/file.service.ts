010c67bc1015b978208fec50b4b7c36714c5e8f0
Raquel <raquel@conecthus.org.br>
2025-01-09 17:18:58 -0400

    feat: leitura de BOM v1

--- backend/src/file/file.service.ts
+++ backend/src/file/file.service.ts
@@ -3,17 +3,32 @@ import * as XLSX from 'xlsx';
 
 @Injectable()
 export class FileService {
-  readExcel(filePath: string): any[] {
-    // Lê o arquivo Excel
+  readExcel(filePath: string) {
     const workbook = XLSX.readFile(filePath);
 
-    // Seleciona a primeira planilha
     const sheetName = workbook.SheetNames[0];
     const worksheet = workbook.Sheets[sheetName];
 
-    // Converte os dados para JSON
-    const data = XLSX.utils.sheet_to_json(worksheet);
+    const jsonData = XLSX.utils.sheet_to_json(worksheet, {
+      header: 1,
+      raw: false,
+    });
 
-    return data; // Retorna os dados como um array de objetos
+    const headerIndex = 5;
+    const headers: any = jsonData[headerIndex];
+    const dataRows = jsonData.slice(headerIndex + 1);
+
+    const processedData = dataRows.map((row) => {
+      const rowObject: any = {};
+      headers.forEach((header, index) => {
+        const value = row[index];
+        if (value !== null && value !== undefined && value !== '') {
+          rowObject[header] = value;
+        }
+      });
+      return rowObject;
+    });
+
+    return processedData.filter((row) => Object.keys(row).length > 0);
   }
 }
