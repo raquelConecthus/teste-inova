8577feb1c53bdf669d5aada05767f98ddf747280
Raquel <raquel@conecthus.org.br>
2025-01-09 17:18:13 -0400

    feat: configuracao CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,6 +7,11 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true,
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
